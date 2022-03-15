4446b6ce2967335b9e47ad7320c0cd0040de44c9
Andrew Goetz <agoetz4407@gmail.com>
2022-03-15 16:15:43 -0500

    added task type to list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,24 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task";
-    tasksToDoEl.appendChild(taskItemEl);
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item div to <ul>
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
