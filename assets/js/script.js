419f6ff89dc38bd166746e78025a614ce0b1b841
Andrew Goetz <agoetz4407@gmail.com>
2022-03-15 15:42:09 -0500

    added submit event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 //Add Task button functionality
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task";
     tasksToDoEl.appendChild(taskItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
